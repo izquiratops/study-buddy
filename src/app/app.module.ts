e7d3caa77e5d4b0de77bf1073e6c5a861fc12037
Jordi Izquierdo <jordi.izquierdo@unblur.co>
2024-02-12 19:38:42 +0100

    Add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
@@ -14,6 +14,7 @@ import { NavigatorBarComponent } from './components/navigator-bar/navigator-bar.
 
 import { HomeService } from '@screens/home/home.service';
 import { EditorService } from '@screens/editor/editor.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,12 @@ import { EditorService } from '@screens/editor/editor.service';
     RouterModule.forRoot(routes),
     FormsModule,
     ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     HomeService,
